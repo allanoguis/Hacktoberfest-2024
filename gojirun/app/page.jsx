cd9406ccfa7e5193282acec0e3adfc53f400212b
Allan Oguis <allanoguis@gmail.com>
2024-10-09 19:51:40 +0800

    file update
    - cleaned fragments
    - updated /server and /client packages
    - no changes from gameControler.js

--- gojirun/app/page.jsx
+++ gojirun/app/page.jsx
@@ -7,24 +7,14 @@ import ContributorsSection from "./sections/contributors";
 
 export default function Home() {
   return (
-
+<>
       <div className="flex flex-col items-center justify-items-center p-0 font-[family-name:var(--font-geist-sans)]">
-        <>
           <Navigation />
-        </>
-        <>
           <Hero />
-        </>
-        <>
           <Game />
-        </>
-        <>
           <ContributorsSection />
-        </>
-        <>
           <Footer />
-        </>
       </div>
-
+      </>
   );
 }
--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,4 +1,4 @@
-import db from '../firebaseConfig.js';
+import db from "../firebaseConfig.js";
 
 export const saveGame = async (req, res) => {
   const { time, score, ipAddress, deviceType, userAgent } = req.body;
@@ -8,20 +8,20 @@ export const saveGame = async (req, res) => {
     console.log(req.body);
 
     // Save the game data to Firestore
-    const docRef = await db.collection('games').add({
+    const docRef = await db.collection("games").add({
       time,
       score,
       ipAddress,
       deviceType,
       userAgent,
-      createdAt: new Date()  // Adding a timestamp
+      createdAt: new Date(), // Adding a timestamp
     });
 
-    console.log('Game saved with ID: ', docRef.id);
+    console.log("Game saved with ID: ", docRef.id);
 
-    res.status(201).json({ message: 'Game saved successfully', id: docRef.id });
+    res.status(201).json({ message: "Game saved successfully", id: docRef.id });
   } catch (error) {
-    console.error('Error in saveGame (server SIDE):', error);
-    res.status(500).json({ message: 'Error saving game' });
+    console.error("Error in saveGame (server SIDE):", error);
+    res.status(500).json({ message: "Error saving game" });
   }
 };
