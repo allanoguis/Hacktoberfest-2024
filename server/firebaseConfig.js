e9ba629adae03896fc7bab775a4cbc7fd83a9de5
Pramod <magadumpramod420@gmail.com>
2024-10-09 17:53:41 +0530

    made a page for all the game score, BUT HAVE not called getGames API

--- server/firebaseConfig.js
+++ server/firebaseConfig.js
@@ -1,5 +1,7 @@
 import admin from 'firebase-admin';
-import serviceAccount from './key.json' assert { type: "json" };
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const serviceAccount = require('./key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
