c201e248c2f0c4917491854f089c1c63ebeae373
Parth <parthbidari13@gmail.com>
2024-10-10 15:16:32 +0530

    working om userdata

--- gojirun/app/sections/navigation.jsx
+++ gojirun/app/sections/navigation.jsx
@@ -47,7 +47,7 @@ export default function Navigation() {
           <UserButton />
         </SignedIn>
         <SignedOut>
-          <SignInButton />
+          <SignInButton mode='modal'/>
         </SignedOut>
 
         {/* Theme Toggle Button */}
